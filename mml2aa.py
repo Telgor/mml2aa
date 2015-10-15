e8e06d331968c024852ec565348368cd0bb61cb6
Telgor <Telgor@users.noreply.github.com>
2015-10-15 16:07:44 +0300

    move lexer to its own file/class.

--- mml2aa.py
+++ mml2aa.py
@@ -1,4 +1,4 @@
-import re
+import mml_lexer
 
 
 def get_note_time(tempo, measure_part):
@@ -11,50 +11,23 @@ def get_note_time(tempo, measure_part):
 strMML = 'r1.rv90l8<arbr4.>crdr4.cr<br4.arg+r4.arbr4.>crdr4.cr<br4.ar>cr4.crcr4.c,eeeee'
 # strMML = raw_input()
 
-voices = strMML.split(",")
+tracks = strMML.split(",")
 
-num_voices = len(voices)
-assert num_voices <= 3
+num_tracks = len(tracks)
+assert num_tracks <= 3
 # TODO: character limit should probably be a command line option.
 char_limit = 200
 
-print voices
+print tracks
 
 
-def lexer(voices):
-    # define a subset of the MML grammar that we want to support:
-    dict_re_elements = {
-        'note': '(?P<extend_note>&)?(?P<Note>[cdefgab])(?P<accidental>[-+#])?(?P<note_measure_part>\d+)?(?P<note_dot>\.)?',
-        'num_note': '(?P<extend_num>&)?(?P<N>n)(?P<Note_num>\d+)(?P<num_note_dot>\.)?',
-        'rest': '(?P<R>[r])(?P<rest_measure_part>\d+)?(?P<rest_dot>\.)?',
-        'default_length': '(?P<L>l)(?P<default_length>\d+)',
-        'volume': '(?P<V>[v])(?P<volume>\d+)',
-        'tempo': '(?P<T>[t])(?P<tempo>\d+)',
-        'octave': '(?P<O>[o])(?P<octave>\d+)',
-        'octave_shift': '(?P<octave_shift>[<>])'
-    }
-
-    str_re_element_grammar = '(' + ')|('.join(dict_re_elements.values()) + ')'
-    re_element = re.compile(str_re_element_grammar)
-    for (i, voice) in enumerate(voices):
-        while True:
-            match_result = re_element.match(voice[positions[i]:])
-            if not match_result:
-                break
-
-            parsed_note = match_result.groupdict(default=None)
-            # remove empty keys
-            parsed_note = dict((k, v) for k, v in parsed_note.iteritems() if v is not None)
-            chars_matched_in_voice = len(match_result.group(0))
-            # debug print
-            print i, ',', chars_matched_in_voice, '@', positions[i], ':', parsed_note
-            positions[i] += chars_matched_in_voice
 
 
 eof = False
-positions = [0] * num_voices
-time = [0] * num_voices
+positions = [0] * num_tracks
+time = [0] * num_tracks
 cur_tempo = 120
-volumes = [100] * num_voices
+volumes = [100] * num_tracks
 
-lexer(voices)
+lexer = mml_lexer.Lexer()
+print lexer.process(tracks)
---
+++ mml_lexer.py
@@ -0,0 +1,44 @@
+import re
+
+
+class Lexer(object):
+    def __init__(self):
+        # define a subset of the MML grammar that we want to support:
+        dict_re_elements = {
+            'note': '(?P<extend_note>&)?(?P<Note>[cdefgab])(?P<accidental>[-+#])?(?P<note_measure_part>\d+)?(?P<note_dot>\.)?',
+            'num_note': '(?P<extend_num>&)?(?P<N>n)(?P<Note_num>\d+)(?P<num_note_dot>\.)?',
+            'rest': '(?P<R>[r])(?P<rest_measure_part>\d+)?(?P<rest_dot>\.)?',
+            'default_length': '(?P<L>l)(?P<default_length>\d+)',
+            'volume': '(?P<V>[v])(?P<volume>\d+)',
+            'tempo': '(?P<T>[t])(?P<tempo>\d+)',
+            'octave': '(?P<O>[o])(?P<octave>\d+)',
+            'octave_shift': '(?P<octave_shift>[<>])'
+        }
+
+        str_re_element_grammar = '(' + ')|('.join(dict_re_elements.values()) + ')'
+        self.re_element = re.compile(str_re_element_grammar)
+
+    def process(self, tracks):
+        ret = []
+        for (i, track) in enumerate(tracks):
+            print 'Track #' + str(i)
+            ret += self.process_tracks(track)
+        return ret
+
+    def process_tracks(self, track):
+        ret = []
+        position = 0
+        while True:
+            match_result = self.re_element.match(track[position:])
+            if not match_result:
+                break
+
+            parsed_note = match_result.groupdict(default=None)
+            # remove empty keys
+            parsed_note = dict((k, v) for k, v in parsed_note.iteritems() if v is not None)
+            chars_matched_in_track = len(match_result.group(0))
+            ret += [parsed_note]
+            # debug print
+            print chars_matched_in_track, '@', position, ':', parsed_note
+            position += chars_matched_in_track
+        return ret
